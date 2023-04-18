323e09bc386b170233bd8d880c4ddab6a7cc8cb5
Ikmal Azman <opikmalazman@gmail.com>
2023-04-18 19:34:02 +0100

    implement and layout UISegmentControl in SwiftUI with Picker object

--- SegmentedControl-SwiftUI/ContentView.swift
+++ SegmentedControl-SwiftUI/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+/// CaseIterable : a Procotol that allow to access the enum as collection type through `.allCases` property.
+enum HeroCharacter : String, CaseIterable, Identifiable {
+    case pikachu = "Pikachu"
+    case eevee = "Eevee"
+    case celebi = "Celebi"
+    var id: Self { self }
+}
+
 struct ContentView: View {
+    init() {
+        /// Allow to ovveride all UISegmentedControl UI appearance in the app
+        UISegmentedControl.appearance().selectedSegmentTintColor = .systemIndigo
+    }
+    
+    @State var selectedCharacter : HeroCharacter = .pikachu
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                segmentedView
+                Spacer()
+                
+                characterImageView
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Choose a Character")
         }
-        .padding()
+    }
+    
+    var segmentedView : some View {
+        /// `Picker`: allow to create UISegmentedControl, UIDropdownMenu, UIPickerView in SwiftUI
+        Picker("Choose a Character", selection: $selectedCharacter) {
+            ForEach(HeroCharacter.allCases) { character in
+                Text(character.rawValue)
+            }
+        }
+        /// `pickerStyle`: a modifier that  allow to style the type of the picker
+        .pickerStyle(.segmented)
+    }
+    
+    var characterImageView : some View {
+        Image(selectedCharacter.rawValue)
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 400)
     }
 }
 
