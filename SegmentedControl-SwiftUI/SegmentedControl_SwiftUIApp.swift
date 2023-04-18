8ee75dd7c09b3bc1809635439afefaadce20360f
Ikmal Azman <opikmalazman@gmail.com>
2023-04-18 19:09:25 +0100

    Initial Commit

---
+++ SegmentedControl-SwiftUI/SegmentedControl_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentedControl_SwiftUIApp.swift
+//  SegmentedControl-SwiftUI
+//
+//  Created by Ikmal Azman on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SegmentedControl_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
